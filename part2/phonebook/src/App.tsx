ffae0dcb822b693c05e6563050f347111109d96e
Alex GH <110792798+alex070822@users.noreply.github.com>
2023-09-14 16:32:01 -0600

    2.11: The Phonebook Step6

--- part2/phonebook/src/App.tsx
+++ part2/phonebook/src/App.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import Filter from "./components/Filter/Filter";
 import PersonForm from "./components/PersonForm/PersonForm";
 import Persons from "./components/Persons/Persons";
+import axios from "axios";
 
 interface Person {
   name: string;
@@ -10,12 +11,7 @@ interface Person {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState<Person[]>([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState<Person[]>([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterText, setFilterText] = useState('');
@@ -47,6 +43,14 @@ const App = () => {
       setNewNumber('');
     }
   };
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+  }, [])
 
   return (
       <div>
