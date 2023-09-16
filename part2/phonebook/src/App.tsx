ff32ef84e2b819ad1189c71d0400deb647f2345b
Alex GH <110792798+alex070822@users.noreply.github.com>
2023-09-15 18:49:10 -0600

    2.13: The Phonebook step8

--- part2/phonebook/src/App.tsx
+++ part2/phonebook/src/App.tsx
@@ -1,19 +1,13 @@
-import {useEffect, useState} from 'react'
-import Filter from "./components/Filter/Filter";
-import PersonForm from "./components/PersonForm/PersonForm";
-import Persons from "./components/Persons/Persons";
-import axios from "axios";
-
-interface Person {
-  name: string;
-  number: string;
-  id: number;
-}
+import { useEffect, useState } from 'react';
+import Filter from './components/Filter/Filter';
+import PersonForm from './components/PersonForm/PersonForm';
+import Persons from './components/Persons/Persons';
+import personsService from '../src/services/persons';
 
 const App = () => {
-  const [persons, setPersons] = useState<Person[]>([])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
   const [filterText, setFilterText] = useState('');
 
   const handleNewName = (e) => {
@@ -35,14 +29,12 @@ const App = () => {
     } else if (persons.some((person) => person.number === newNumber)) {
       alert(`${newNumber} is already added to phonebook`);
     } else {
+      const newPerson = { name: newName, number: newNumber };
 
-      const newPerson = {name: newName, number: newNumber};
-
-      axios
-      .post('http://localhost:3001/persons', newPerson)
+      personsService
+      .create(newPerson)
       .then((response) => {
-
-        setPersons([...persons, response.data]);
+        setPersons([...persons, response]);
         setNewName('');
         setNewNumber('');
       })
@@ -50,26 +42,31 @@ const App = () => {
         console.error('Error adding person:', error);
       });
     }
-  }
+  };
 
   useEffect(() => {
-    axios
-    .get('http://localhost:3001/persons')
-    .then(response => {
-      setPersons(response.data)
-    })
-  }, [])
+    personsService.getAll()
+    .then((response) => {
+      setPersons(response);
+    });
+  }, []);
 
   return (
       <div>
         <h2>Phonebook</h2>
-        <Filter filterText={filterText} handleFilterChange={handleFilterChange}/>
+        <Filter filterText={filterText} handleFilterChange={handleFilterChange} />
         <h3>add a new</h3>
-        <PersonForm handleSubmit={handleSubmit} newName={newName} handleNewName={handleNewName} newNumber={newNumber} handleNewNumber={handleNewNumber}/>
+        <PersonForm
+            handleSubmit={handleSubmit}
+            newName={newName}
+            handleNewName={handleNewName}
+            newNumber={newNumber}
+            handleNewNumber={handleNewNumber}
+        />
         <h3>Numbers</h3>
-        <Persons persons={persons} filterText={filterText}/>
+        <Persons persons={persons} filterText={filterText} />
       </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
