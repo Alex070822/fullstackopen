23e9ab78cb7f185acf8df4f88f8c34ea67063404
Alex GH <110792798+alex070822@users.noreply.github.com>
2023-10-06 20:43:08 -0600

    3.9 phonebook backend step9

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('custom', (req) => {
   if (req.body && req.body.name && req.body.number) {
@@ -10,6 +11,7 @@ morgan.token('custom', (req) => {
   }
 });
 
+app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms {:custom}'));
 app.use(express.json())
 
