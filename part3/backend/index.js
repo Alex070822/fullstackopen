59998196e53548174a945db83d6837fb8d168e37
Alex GH <110792798+alex070822@users.noreply.github.com>
2023-10-12 20:01:15 -0600

    3.15: Phonebook database, step3

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -64,53 +64,57 @@ app.get('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: 'name or number missing'
-    })
-  }
-
-  if (persons.some(person => person.name === body.name)) {
-    return response.status(409).json({
-      error: 'name already exists'
-    })
-  }
-
-  const person = new Person({
-    name: body.name,
-    number: body.number,
+/*
+if (!body.name || !body.number) {
+  return response.status(400).json({
+    error: 'name or number missing'
   })
+}
+
+if (persons.some(person => person.name === body.name)) {
+  return response.status(409).json({
+    error: 'name already exists'
+  })
+}
+*/
+
+const person = new Person({
+  name: body.name,
+  number: body.number,
+})
 
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(note => note.id !== id)
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
-  const currentTime = new Date();
-  const options = {
-    weekday: 'short',
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    hour12: false,
-    timeZoneName: 'long',
-  };
+const currentTime = new Date();
+const options = {
+  weekday: 'short',
+  year: 'numeric',
+  month: 'short',
+  day: 'numeric',
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit',
+  hour12: false,
+  timeZoneName: 'long',
+};
 
-  const formattedTime = new Intl.DateTimeFormat('en-US', options).format(currentTime);
-  response.send(`<p>Phonebook has info ${persons.length} for people</p><p>${formattedTime}</p>`)
+const formattedTime = new Intl.DateTimeFormat('en-US', options).format(currentTime);
+response.send(`<p>Phonebook has info ${persons.length} for people</p><p>${formattedTime}</p>`)
 })
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+console.log(`Server running on port ${PORT}`)
 })
