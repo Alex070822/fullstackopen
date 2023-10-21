c19a0c716a25fe6d5f6bfcbb971c1285888539d3
Alex GH <110792798+alex070822@users.noreply.github.com>
2023-10-20 18:36:49 -0600

    3.22: Lint configuration

--- part3/backend/models/person.js
+++ part3/backend/models/person.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config()
 const mongoose = require('mongoose')
 
 const url = process.env.MONGODB_URI
@@ -7,20 +7,20 @@ console.log('connecting to', url)
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
-.then(result => {
-  console.log('connected to MongoDB')
-})
-.catch((error) => {
-  console.log('error connecting to MongoDB:', error.message)
-})
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const phoneNumberValidator = {
   validator: function (value) {
-    const pattern = /^[0-9]{2,3}-[0-9]+$/;
-    return pattern.test(value);
+    const pattern = /^[0-9]{2,3}-[0-9]+$/
+    return pattern.test(value)
   },
   message: 'Invalid format. The number should be formed of two parts that are separated by a hyphen, the first part has two or three numbers and the second part also consists of numbers.',
-};
+}
 
 const personSchema = new mongoose.Schema({
   name: {
