57c8b27cfe500360970ed54521d40f67b94d72d5
Alex GH <110792798+alex070822@users.noreply.github.com>
2023-10-23 19:53:30 -0600

    4.2 Blog list, step2

---
+++ part4/backend/app.js
@@ -0,0 +1,29 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/entries')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery',false)
+
+const mongoUrl = config.MONGODB_URI
+
+logger.info('connecting to', mongoUrl)
+
+mongoose.connect(mongoUrl)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
--- part4/backend/models/blog.js
+++ part4/backend/models/blog.js
@@ -1,18 +1,4 @@
-require('dotenv').config()
 const mongoose = require('mongoose')
-
-const mongoUrl = process.env.MONGODB_URI
-
-console.log('connecting to', mongoUrl)
-
-mongoose.set('strictQuery',false)
-mongoose.connect(mongoUrl)
-.then(() => {
-  console.log('connected to MongoDB')
-})
-.catch((error) => {
-  console.log('error connecting to MongoDB:', error.message)
-})
 
 const blogSchema = new mongoose.Schema({
   title: String,
