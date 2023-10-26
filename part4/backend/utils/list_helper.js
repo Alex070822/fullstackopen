c9639c702f21b46b9f77f10308f7a206f9aa72f0
Alex GH <110792798+alex070822@users.noreply.github.com>
2023-10-25 18:23:54 -0600

    4.5*: helper functions and unit tests, step3

--- part4/backend/utils/list_helper.js
+++ part4/backend/utils/list_helper.js
@@ -2,7 +2,33 @@ const dummy = () => 1
 
 const totalLikes = (blogs) => blogs.reduce((sum, obj) => sum + obj.likes, 0)
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+  if (blogs.length === 1) {
+    const blog = blogs[0]
+    return {
+      title: blog.title,
+      author: blog.author,
+      likes: blog.likes,
+    }
+  }
+
+  const maxLikes = Math.max(...blogs.map(blog => blog.likes))
+  const favoriteBlogs = blogs.filter(blog => blog.likes === maxLikes)
+
+  const mostLiked = favoriteBlogs[0]
+
+  return {
+    title: mostLiked.title,
+    author: mostLiked.author,
+    likes: mostLiked.likes,
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
